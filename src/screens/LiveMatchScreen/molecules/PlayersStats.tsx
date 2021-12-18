b3f254b728308862786a079dc2ab31a6a8ed6558
naveen_gts <naveen.gtech20@gmail.com>
2021-12-18 13:54:02 +0530

    player stats scroll

--- src/screens/LiveMatchScreen/molecules/PlayersStats.tsx
+++ src/screens/LiveMatchScreen/molecules/PlayersStats.tsx
@@ -6,78 +6,101 @@ import assets from '../../../constants/assets_manifest';
 import LinearGradient from 'react-native-linear-gradient';
 import {BottomLine} from '../../../sharedComponents';
 import {useNavigation} from '@react-navigation/native';
-
+// @ts-ignore
+import contestPlayerStats from '../../../constants/mocks/contestPlayerStats.json';
 interface PropTypes {
   text?: string;
 }
 
 export default function PlayersStats(props: PropTypes) {
+  const navigation = useNavigation<any>();
+
   return (
     <View>
-      <ScrollView style={[tailwind('')]}>
-        <Filter />
-        <TableRow />
-        <PlayerStats name="Virat Kohli" />
-        <PlayerStats name="R Sharma" />
-        <PlayerStats name="M Dhoni" />
-        <PlayerStats name="Virat Kohli" />
-        <PlayerStats name="R Sharma" />
-        <PlayerStats name="M Dhoni" />
-        <View style={[tailwind('h-10')]}></View>
-      </ScrollView>
+      <Filter />
+
+      <View style={[tailwind('flex-row bg-dark-3')]}>
+        <View style={[tailwind('')]}>
+          <View style={[tailwind('bg-dark-4 p-2'), {width: 130}]}>
+            <Text style={[tailwind('font-regular text-dark-1 px-2 font-12')]}>
+              PLAYERS
+            </Text>
+          </View>
+
+          {contestPlayerStats.map((item: any) => {
+            return (
+              <TouchableOpacity
+                onPress={() => navigation.navigate('MyContestPlayersScreen')}
+                style={[tailwind('flex-row items-center mt-2'), {width: 130}]}>
+                <Image
+                  resizeMode="contain"
+                  source={assets.player}
+                  style={[{width: 36, height: 36}]}
+                />
+                <View style={[tailwind('px-2')]}>
+                  <Text
+                    numberOfLines={1}
+                    style={[
+                      tailwind(
+                        'font-bold text-white capitalize font-14 py-0.5',
+                      ),
+                    ]}>
+                    {item.name}
+                  </Text>
+                  <Text style={[tailwind('font-regular text-dark-1 font-11')]}>
+                    IND-BAT
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={[tailwind('border-l  border-gray-700')]}>
+          <View>
+            <TableRow />
+            {contestPlayerStats.map((item: any) => {
+              return (
+                <TouchableOpacity
+                  activeOpacity={0.5}
+                  onPress={() => navigation.navigate('MyContestPlayersScreen')}
+                  style={[tailwind('flex-row flex-grow items-center mt-2')]}>
+                  <View style={[tailwind('px-2'), {width: 62}]}>
+                    <Text
+                      style={[tailwind('font-regular text-dark-1 font-14')]}>
+                      {item.points}
+                    </Text>
+                  </View>
+                  <View style={[tailwind(''), {width: 62}]}>
+                    <Text
+                      style={[tailwind('font-regular text-dark-1 font-14')]}>
+                      {item.sell_by} %
+                    </Text>
+                  </View>
+                  <View style={[tailwind(''), {width: 62}]}>
+                    <Text
+                      style={[tailwind('font-regular text-dark-1 font-14')]}>
+                      {item.c_by} %
+                    </Text>
+                  </View>
+                  <View style={[tailwind(''), {width: 62}]}>
+                    <Text
+                      style={[tailwind('font-regular text-dark-1 font-14')]}>
+                      {item.c_by} %
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
-
-const PlayerStats = (props: any) => {
-  const navigation = useNavigation<any>();
-  return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate('MyContestPlayersScreen')}>
-      <View style={[tailwind('flex-row pt-3 bg-dark-3 items-center')]}>
-        <View style={[tailwind('flex-row items-center'), {flex: 4}]}>
-          <View style={[tailwind('px-2')]}>
-            <Image
-              resizeMode="contain"
-              source={assets.player}
-              style={[{width: 36, height: 36}]}
-            />
-          </View>
-
-          <View>
-            <Text
-              numberOfLines={1}
-              style={[tailwind('font-bold text-light font-14')]}>
-              {props.name}
-            </Text>
-            <Text style={[tailwind('font-regular text-dark-1 font-12 pt-1')]}>
-              IND - BAT
-            </Text>
-          </View>
-        </View>
-
-        <View style={[tailwind(''), {flex: 2}]}>
-          <Text style={[tailwind('font-regular text-dark-1 font-14 py-2')]}>
-            34.3%
-          </Text>
-        </View>
-        <View style={[tailwind(''), {flex: 2}]}>
-          <Text style={[tailwind('font-regular text-dark-1 font-14 py-2')]}>
-            34.3%
-          </Text>
-        </View>
-        <View style={[tailwind(''), {flex: 2}]}>
-          <Text style={[tailwind('font-regular text-dark-1 font-14 py-2')]}>
-            34.3%
-          </Text>
-        </View>
-      </View>
-      <View>
-        <BottomLine />
-      </View>
-    </TouchableOpacity>
-  );
-};
 
 const Filter = () => {
   return (
@@ -121,38 +144,41 @@ const Filter = () => {
 
 const TableRow = () => {
   return (
-    <View style={[tailwind('flex-row items-center pb-4 pt-1')]}>
+    <View style={[tailwind('flex-row items-center bg-dark-4 py-2 pl-1')]}>
       <View
         style={[
           tailwind('flex-row justify-between items-center'),
-          {flex: 3.5},
+          {width: 62},
         ]}>
-        <Text
-          style={[tailwind('font-regular text-dark-1 font-12 uppercase px-4')]}>
-          Players
+        <Text style={[tailwind('font-regular text-dark-1 font-12 uppercase')]}>
+          Points
         </Text>
       </View>
+
       <View
-        style={[tailwind('flex-row justify-between items-center'), {flex: 2}]}>
-        <Text
-          style={[tailwind('font-regular text-dark-1 font-12 uppercase px-4')]}>
+        style={[
+          tailwind('flex-row justify-between items-center'),
+          {width: 62},
+        ]}>
+        <Text style={[tailwind('font-regular text-dark-1 font-12 uppercase')]}>
           L BY
         </Text>
       </View>
       <View
-        style={[tailwind('flex-row  justify-between items-center'), {flex: 2}]}>
-        <Text
-          style={[tailwind('font-regular text-dark-1 font-12 uppercase px-4')]}>
+        style={[
+          tailwind('flex-row  justify-between items-center'),
+          {width: 62},
+        ]}>
+        <Text style={[tailwind('font-regular text-dark-1 font-12 uppercase')]}>
           %C by
         </Text>
       </View>
       <View
         style={[
           tailwind('flex-row justify-between items-center'),
-          {flex: 2.5},
+          {width: 62},
         ]}>
-        <Text
-          style={[tailwind('font-regular text-dark-1 font-12 uppercase px-4')]}>
+        <Text style={[tailwind('font-regular text-dark-1 font-12 uppercase')]}>
           % VC BY
         </Text>
       </View>
